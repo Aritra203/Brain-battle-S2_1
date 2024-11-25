4c2d7d129f6e351107f4a73fdddf90bf5d5954bc
Souvik <souvik13109@gmail.com>
2024-11-25 20:42:47 +0530

    Pre final draft

--- script.js
+++ script.js
@@ -188,7 +188,7 @@ gsap.to(imageSeq, {
   ease: `none`,
   scrollTrigger: {
     scrub: 0.15,
-    trigger: `#page>canvas`,
+    trigger: `#part>canvas`,
     start: `top top`,
     end: `300% top`,
     scroller: `#main`,
@@ -223,7 +223,7 @@ function scaleImage(img, ctx) {
   );
 }
 ScrollTrigger.create({
-  trigger: "#page>canvas",
+  trigger: "#part>canvas",
   pin: true,
   // markers:true,
   scroller: `#main`,
@@ -260,3 +260,66 @@ const targetDate = new Date("December 16, 2024").getTime();
                 document.getElementById("countdown").style.display = "none"; // Optionally hide the countdown
             }
         }, 1000);
+
+        const scroll = new LocomotiveScroll({
+          el: document.querySelector('#main'),
+          smooth: true
+      });
+      
+
+        function page4Animation() {
+          var elemC = document.querySelector("#elem-container")
+          var fixed = document.querySelector("#fixed-image")
+          elemC.addEventListener("mouseenter", function () {
+              fixed.style.display = "block"
+          })
+          elemC.addEventListener("mouseleave", function () {
+              fixed.style.display = "none"
+          })
+      
+          var elems = document.querySelectorAll(".elem")
+          elems.forEach(function (e) {
+              e.addEventListener("mouseenter", function () {
+                  var image = e.getAttribute("data-image")
+                  fixed.style.backgroundImage = `url(${image})`
+              })
+          })
+      }
+      
+      function swiperAnimation() {
+          var swiper = new Swiper(".mySwiper", {
+              slidesPerView: "auto",
+              centeredSlides: true,
+              spaceBetween: 100,
+          });
+      }
+      function menuAnimation() {
+      
+          var menu = document.querySelector("nav h3")
+          var full = document.querySelector("#full-scr")
+          var navimg = document.querySelector("nav img")
+          var flag = 0
+          menu.addEventListener("click", function () {
+              if (flag == 0) {
+                  full.style.top = 0
+                  navimg.style.opacity = 0
+                  flag = 1
+              } else {
+                  full.style.top = "-100%"
+                  navimg.style.opacity = 1
+                  flag = 0
+              }
+          })
+      }
+      
+      function loaderAnimation() {
+          var loader = document.querySelector("#loader")
+          setTimeout(function () {
+              loader.style.top = "-100%"
+          }, 4200)
+      }
+      
+      swiperAnimation()
+      page4Animation()
+      menuAnimation()
+      loaderAnimation()
